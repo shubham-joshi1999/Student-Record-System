d7510c887ed160bf2e440f26c3b36e76d78f5f64
Shubham Joshi <shubham.joshi041999@gmail.com>
2019-07-11 14:44:52 +0530

    Update server.js

--- student_management_system/server.js
+++ student_management_system/server.js
@@ -1,7 +1,7 @@
 const express = require('express') ;
 const srv = express() ;  
 const path = require('path')
-
+const Server_Port = process.env.PORT || 1234 ;
 srv.use(express.json()) ; 
 srv.use(express.urlencoded({extended:true})) ;  
 
@@ -9,8 +9,8 @@ srv.use('/students',require('./routes/students').route) ;
 
 srv.use('/',express.static(path.join(__dirname + '/public'))) ;
 
-srv.listen(1234 , ()=>
+srv.listen(Server_Port , ()=>
 {
-    console.log("Server started on http://localhost:1234/") ; 
+   // console.log("Server started on http://localhost:1234/") ; 
 })
 
